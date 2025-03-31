88c66848553efd01a078c23d404fde1a77edb4f3
Eugenio Zucchi <ez.zucchieugenio@gmail.com>
2025-03-31 12:13:47 -0300

    First hand-in

--- change.py
+++ change.py
@@ -1,3 +1,6 @@
 def change():
     expense = 23.75
     money = 100
+    change = money - expense
+    print(f"Ingresar gasto:\n23.75\nDinero recibido\n100\n\nVuelto\n\nPesos:\n{int(change)}\nCentavos:\n{int((change - int(change))*100)}")
+change()
